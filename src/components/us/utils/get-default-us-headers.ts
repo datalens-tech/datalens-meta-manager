677e111f54f982039a378e331d6623954b43b773
3Tn0 <v.vl.stepanenko@gmail.com>
2025-04-02 13:48:59 +0300

    Fix tenant id proxy

--- src/components/us/utils/get-default-us-headers.ts
+++ src/components/us/utils/get-default-us-headers.ts
@@ -7,15 +7,16 @@ export const getDefaultUsHeaders = (
     ctx: AppContext,
 ): {
     [AUTHORIZATION_HEADER]?: string;
-    [TENANT_ID_HEADER]: string;
+    [TENANT_ID_HEADER]?: string;
 } => {
     const user = getCtxUser(ctx);
     const info = getCtxInfo(ctx);
 
     return {
-        [AUTHORIZATION_HEADER]: user?.accessToken
-            ? `${DL_AUTH_HEADER_KEY} ${user.accessToken}`
-            : undefined,
-        [TENANT_ID_HEADER]: info.tenantId,
+        ...(user?.accessToken
+            ? {[AUTHORIZATION_HEADER]: `${DL_AUTH_HEADER_KEY} ${user.accessToken}`}
+            : {}),
+
+        ...(info.tenantId ? {[TENANT_ID_HEADER]: info.tenantId} : {}),
     };
 };
