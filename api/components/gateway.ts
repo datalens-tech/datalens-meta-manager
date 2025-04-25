bd11dc0a7633edfde98f8a3526a7376cbd01eaee
3Tn0 <v.vl.stepanenko@gmail.com>
2025-04-25 16:27:49 +0300

    Add api exports

---
+++ api/components/gateway.ts
@@ -0,0 +1,4 @@
+export {getGatewayConfig} from '../../src/components/gateway';
+
+export {us as usSchema} from '../../src/components/gateway/schema/us';
+export {uiApi as uiApiSchema} from '../../src/components/gateway/schema/ui-api';
---
+++ api/components/index.ts
@@ -0,0 +1,7 @@
+export {initSwagger} from '../../src/components/api-docs';
+
+export {setRegistryToContext} from '../../src/components/app-context';
+
+export {prepareErrorResponse} from '../../src/components/error/error-response-presenter';
+
+export {initTemporal} from '../../src/components/temporal/utils';
