8688752d17215cdea25e3b1c62e451385ff3ee52
Georgy Malkov <malkovgv@yandex-team.ru>
2025-05-20 18:31:15 +0300

    fix worker auth

--- src/components/temporal/client/client.ts
+++ src/components/temporal/client/client.ts
@@ -1,35 +1,15 @@
 import {Client, Connection} from '@temporalio/client';
-import jwt from 'jsonwebtoken';
 
-import {getEnvCert, isTruthyEnvVariable} from '../../../utils';
 import {NAMESPACE} from '../constants';
+import {getApiKey} from '../utils';
 
 let _client: Client;
 
 const initClient = async () => {
     if (!_client) {
-        let apiKey: string | undefined;
-
-        if (isTruthyEnvVariable('TEMPORAL_AUTH_ENABLED') && process.env.TEMPORAL_AUTH_PRIVATE_KEY) {
-            const authPrivateKey = getEnvCert('TEMPORAL_AUTH_PRIVATE_KEY') || '';
-            apiKey = jwt.sign(
-                {
-                    sub: process.env.TEMPORAL_AUTH_SERVICE || 'temporal',
-                    permissions: `["${NAMESPACE}:admin"]`,
-                },
-                authPrivateKey,
-                {
-                    algorithm: 'RS256',
-                    // PS256 not supported by default at temporal
-                    // https://github.com/temporalio/temporal/blob/main/common/authorization/default_token_key_provider.go#L63
-                    keyid: process.env.TEMPORAL_AUTH_SERVICE || 'temporal',
-                },
-            );
-        }
-
         const connection = await Connection.connect({
             address: process.env.TEMPORAL_ENDPOINT,
-            apiKey,
+            apiKey: getApiKey(),
         });
 
         _client = new Client({connection, namespace: NAMESPACE});
---
+++ src/components/temporal/utils/api-key.ts
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+import {getEnvCert, isTruthyEnvVariable} from '../../../utils';
+import {NAMESPACE} from '../constants';
+
+export const getApiKey = (): string | undefined => {
+    if (isTruthyEnvVariable('TEMPORAL_AUTH_ENABLED') && process.env.TEMPORAL_AUTH_PRIVATE_KEY) {
+        const authPrivateKey = getEnvCert('TEMPORAL_AUTH_PRIVATE_KEY') || '';
+        return jwt.sign(
+            {
+                sub: NAMESPACE,
+                permissions: [`${NAMESPACE}:admin`],
+            },
+            authPrivateKey,
+            {
+                algorithm: 'RS256',
+                // PS256 not supported by default at temporal
+                // https://github.com/temporalio/temporal/blob/main/common/authorization/default_token_key_provider.go#L63
+                keyid: process.env.TEMPORAL_AUTH_SERVICE || 'temporal',
+            },
+        );
+    }
+    return undefined;
+};
