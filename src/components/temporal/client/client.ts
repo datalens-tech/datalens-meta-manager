a64697d3bbe97a0ba30114c906fe1d3e93b437cd
Georgy Malkov <malkovgv@yandex-team.ru>
2025-05-20 14:33:41 +0300

    Add jwt key auth verification

--- src/components/temporal/client/client.ts
+++ src/components/temporal/client/client.ts
@@ -1,12 +1,31 @@
 import {Client, Connection} from '@temporalio/client';
+import jwt from 'jsonwebtoken';
 
+import {isTruthyEnvVariable} from '../../../utils';
 import {NAMESPACE} from '../constants';
 
 let _client: Client;
 
 const initClient = async () => {
     if (!_client) {
-        const connection = await Connection.connect({address: process.env.TEMPORAL_ENDPOINT});
+        let apiKey: string | undefined;
+        if (isTruthyEnvVariable('TEMPORAL_AUTH_ENABLED') && process.env.TEMPORAL_AUTH_PRIVATE_KEY) {
+            apiKey = jwt.sign(
+                {
+                    sub: process.env.TEMPORAL_AUTH_SERVICE || 'temporal',
+                    permissions: `["${NAMESPACE}:admin"]`,
+                },
+                process.env.TEMPORAL_AUTH_PRIVATE_KEY,
+                {
+                    algorithm: 'RS256',
+                    keyid: process.env.TEMPORAL_AUTH_SERVICE || 'temporal',
+                },
+            );
+        }
+        const connection = await Connection.connect({
+            address: process.env.TEMPORAL_ENDPOINT,
+            apiKey,
+        });
 
         _client = new Client({connection, namespace: NAMESPACE});
     }
